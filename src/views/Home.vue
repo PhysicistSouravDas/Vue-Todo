7bf471d899773791b3b078356617b62f2cb073b7
PhysicistSouravDas <souravdas.sd20@gmail.com>
2020-09-07 23:59:57 +0530

    Removed unnecessary css :fire:

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -45,29 +45,3 @@ export default {
   }
 }
 </script>
-
-<style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-
-  body  {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
-
-  .btn  {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
-
-  .btn:hover  {
-    background: #666;
-  }
-</style>
