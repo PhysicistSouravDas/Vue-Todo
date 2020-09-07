19c9f1355a36d454ab7e0c68ecbbad49cd120dd3
PhysicistSouravDas <souravdas.sd20@gmail.com>
2020-09-07 23:55:02 +0530

    Added padding :lipstick:

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="about">
-    <h1>About</h1>
-    <p>This is the TodoList app v1.0.0</p>
-  </div>
+    <div class="about">
+        <h1>About</h1>
+        <p>This is the TodoList app v1.0.0</p>
+    </div>
 </template>
+
+<style scoped>
+    .about  {
+        padding: 10px;
+    }
+</style>
