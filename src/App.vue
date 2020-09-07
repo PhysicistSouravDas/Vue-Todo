d9603d27b9bd680c4476b8971a7c8ce3d3f6c80e
PhysicistSouravDas <souravdas.sd20@gmail.com>
2020-09-07 23:22:12 +0530

    Added unique header for all routes :lipstick:

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <Header />
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
+
+<script>
+import Header from './components/layout/header';
+export default {
+  name: "app",
+  components: {
+    Header
+  }
+}
+</script>
 
 <style>
   /* #app {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
   <div id="app">
-    <Header />
     <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import Header from '../components/layout/header.vue';
 import Todos from '../components/Todos.vue';
 import AddTodo from '../components/AddTodo.vue';
 import axios from 'axios';
@@ -15,7 +13,6 @@ import axios from 'axios';
 export default {
   name: 'Home',
   components: {
-    Header,
     Todos,
     AddTodo
   },
