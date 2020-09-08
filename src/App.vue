808ac6e0b7ac62203b0510792ed156e17bfac7cd
PhysicistSouravDas <souravdas.sd20@gmail.com>
2020-09-09 00:01:43 +0530

    Indent using 4 spaces instead of 2

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,66 @@
 <template>
-  <div id="app">
-    <Header />
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-    <router-view/>
-  </div>
+    <div id="app">
+        <Header />
+        <!-- <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+        </div> -->
+        <router-view/>
+    </div>
 </template>
 
 <script>
 import Header from './components/layout/header';
 export default {
-  name: "app",
-  components: {
-    Header
-  }
+    name: "app",
+    components: {
+        Header
+    }
 }
 </script>
 
 <style>
-  /* #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
+    /* #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 
-  #nav {
-    padding: 30px;
-  }
+    #nav {
+        padding: 30px;
+    }
 
-  #nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
 
-  #nav a.router-link-exact-active {
-    color: #42b983;
-  } */
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    } */
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
 
-  body  {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
+    body  {
+        font-family: Arial, Helvetica, sans-serif;
+        line-height: 1.4;
+    }
 
-  .btn  {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
+    .btn  {
+        display: inline-block;
+        border: none;
+        background: #555;
+        color: #fff;
+        padding: 7px 20px;
+        cursor: pointer;
+    }
 
-  .btn:hover  {
-    background: #666;
-  }
+    .btn:hover  {
+        background: #666;
+    }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app">
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
-  </div>
+    <div id="app">
+        <AddTodo v-on:add-todo="addTodo" />
+        <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    </div>
 </template>
 
 <script>
@@ -11,37 +11,37 @@ import AddTodo from '../components/AddTodo.vue';
 import axios from 'axios';
 
 export default {
-  name: 'Home',
-  components: {
-    Todos,
-    AddTodo
-  },
-  data()  {
-    return  {
-      todos: []
-    }
-  },
-  methods:  {
-    deleteTodo(id)  {
-      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-        // eslint-disable-next-line
-        .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
-        .catch(err => console.log(err));
+    name: 'Home',
+    components: {
+        Todos,
+        AddTodo
     },
-    addTodo(newTodo) {
-      const { title, completed } = newTodo;
-      axios.post('https://jsonplaceholder.typicode.com/todos', {
-        title,
-        completed
-      })
-        .then(res => this.todos = [...this.todos, res.data])
-        .catch(err => console.log(err));
+    data()  {
+        return  {
+          todos: []
+        }
+    },
+    methods:  {
+        deleteTodo(id)  {
+            axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+                // eslint-disable-next-line
+                .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+                .catch(err => console.log(err));
+        },
+        addTodo(newTodo) {
+            const { title, completed } = newTodo;
+            axios.post('https://jsonplaceholder.typicode.com/todos', {
+                title,
+                completed
+            })
+                .then(res => this.todos = [...this.todos, res.data])
+                .catch(err => console.log(err));
+        }
+    },
+    created() {
+        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+            .then(res => this.todos = res.data)
+            .catch(err =>console.log(err));
     }
-  },
-  created() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
-      .then(res => this.todos = res.data)
-      .catch(err =>console.log(err));
-  }
 }
 </script>
